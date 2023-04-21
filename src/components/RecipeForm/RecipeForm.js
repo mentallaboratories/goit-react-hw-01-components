cb2c6c444b947cdfc7d7b5c6dd45bc61b754b228
mentallaboratories <mentallaboratories@gmail.com>
2023-04-21 23:27:02 +0300

    form layout and style

--- src/components/RecipeForm/RecipeForm.js
+++ src/components/RecipeForm/RecipeForm.js
@@ -7,6 +7,31 @@ export const RecipeForm = () =>{
                 Name
                 <input type='text' name='name'/>
             </FormField>
+            <FormField>
+                Image
+                <input type='text' name='image'/>
+            </FormField>
+            <FormField>
+                Time
+            <input type='number' name='time'/>
+            </FormField>
+            <FormField>
+                Servings
+                <input type='number' name='servings'/>
+            </FormField>
+            <FormField>
+                Calories
+                <input type='number' name='calories'/>
+            </FormField>
+            <FormField>
+                Difficulty
+                <select name="difficulty">
+                    <option value="easy">Easy</option>
+                    <option value="medium">Medium</option>
+                    <option value="hard">Hard</option>
+                </select>
+            </FormField>
+            <button type="submit">Save recipe</button>
         </Form>
     )
 }
--- src/components/RecipeForm/RecipeForm.styled.js
+++ src/components/RecipeForm/RecipeForm.styled.js
@@ -1,13 +1,17 @@
 import styled  from "styled-components";
 
 export const Form = styled.form`
-    width: 340px;
-    height: 210px;
+    width: 400px;
+    padding: 8px;
+    border: 1px solid #2a2a2a;
+    display: flex;
+    flex-direction: column;
+    gap:12px;
     background-color: pink;
 `;
 
-export const FormField = styled.fieldset`
-    width: 340px;
-    height: 210px;
-    background-color: red;
+export const FormField = styled.label`
+    display: flex;
+    flex-direction: column;
+    background-color: tomato;
 `;
